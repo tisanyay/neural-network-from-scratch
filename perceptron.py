f6a314d0174d96815241de74a571cd91cb905dd3
Yisan Tay <brendentay@meridian.jwong.dev>
2025-02-24 02:09:16 +0800

    removed files

--- perceptron.py
+++ perceptron.py
@@ -10,60 +10,97 @@ def accuracy(X, y):
 
     return ((np.count_nonzero(predictions == y)) / len(predictions)) * 100
 
-class Perceptron:
-    def __init__(self, batch_size):
-        self.batch_size = batch_size
-        self.l1 = np.random.rand(784, 16) * np.sqrt(2/784)
-        self.b1 = np.zeros((batch_size, 16))
-        self.l2 = np.random.rand(16, 16) * np.sqrt(2/16)
-        self.b2 = np.zeros((batch_size, 16))
-        self.l3 = np.random.rand(16, 10)  * np.sqrt(2/16)
-        self.b3 = np.zeros((batch_size, 10))
+class Softmax:
+    def feed_forward(self, x):
+        x = x.T
+        s = np.exp(x) / sum(np.exp(x))
+        return s.T
+    
+    def derivative(self, X):
+        return X
+    
+    def update_gradients(self, alpha):
+        return
 
-        self.layers = [self.l1, self.l2, self.l3]
-        self.biases = [self.b1, self.b2, self.b3]
-        self.outputs = []
+class ReLU:
+    def __init__(self):
+        self.output = 0
+
+    def feed_forward(self, X):
+        self.output = np.maximum(0, X)
+        return self.output
+    
+    def derivative(self, error):
+        return error * ((self.output > 0) * 1) 
+
+    def update_gradients(self, alpha):
+        return 
+
+class Linear:
+    def __init__(self, input, output):
+        self.weights = np.random.randn(input, output) 
+        self.biases = np.random.randn(1, output) 
+        self.input = None
+        self.m_gradient = None
+        self.b_gradient = None 
     
     def feed_forward(self, X):
-        m, n = X.shape
-        self.outputs = [X]
+        self.input = X
+        return X @ self.weights + self.biases
+    
+    def derivative(self, error):
+        self.b_gradient = 1/1000 * np.sum(error, axis=0)
+        self.m_gradient = 1/1000 * self.input.T @ error
+        return error @ self.weights.T
+    
+    def update_gradients(self, alpha):
+        self.weights -= alpha * self.m_gradient
+        self.biases -= alpha * self.b_gradient
+        return
 
-        for layer, bias in zip(self.layers, self.biases):
-            X = X @ layer
-            X = X + bias
-            X = np.maximum(0, X)
+class NeuralNetwork:
+    def __init__(self):
+        self.components = [
+            Linear(28*28, 128),
+            ReLU(),
+            Linear(128, 16),
+            ReLU(),
+            # Linear(16, 16),
+            # ReLU(),
+            # Linear(16, 16),
+            # ReLU(),
+            Linear(16, 10),
+            Softmax(),
+        ]
+        self.outputs = []
+
+    def feed_forward(self, X):
+        self.outputs.append(X)
+        for component in self.components:
+            X = component.feed_forward(X)
             self.outputs.append(X)
-
         return X
     
     def backpropagate(self, y, alpha):
-        prediction = self.outputs.pop()
-        error =  (prediction - y) / y.shape[0]
+        error = self.outputs.pop() - y
+        for component in self.components[::-1]:
+            error = component.derivative(error)
+            component.update_gradients(alpha) 
 
-        for output, layer, bias in zip(self.outputs[::-1], self.layers[::-1], self.biases[::-1]):
-            layer -= alpha * (output.T @ error) 
-            print('bias:', bias.shape)
-            print('error:', error.shape)
-            bias -= alpha * error 
-            error = (np.maximum(0, layer @ error.T).T) 
-        
-train_size = 2000
+train_size = 5000
+batch_size = 248
 test_size = 100
-batch_size = 128
-iteration = 20
+iterations = 200
+alpha = 1e-2
 
+nn = NeuralNetwork()
 dataloader = Dataloader(train_size, test_size, batch_size)
-perceptron = Perceptron(batch_size)
 
-for _ in range(iteration):
+for i in range(iterations):
     X, y = dataloader.load_batch()
-    predictions = perceptron.feed_forward(X)
-    # print(mse(predictions, y))
-    # print(str(round(accuracy(predictions, y), 4)) + "%")
-    print(predictions)
-    perceptron.backpropagate(y, 0.000001)
+    prediction = nn.feed_forward(X)
+    nn.backpropagate(y, alpha)
+    
+    if i % 10 == 0:
+        print(str(i) + 'th iteration: ' + str(round(accuracy(prediction, y), 4)) + "%")
 
-
-# test_X, test_y = dataloader.load_test()
-# test_prediction = perceptron.feed_forward(test_X)
-# print(str(accuracy(test_prediction, test_y)) + "%")
